e613a39acd3cee99a23b88357eed786d7684db63
Nigel Squire <squiren72@gmail.com>
2022-03-06 10:08:08 +0000

    NS: Added another class to add post

--- hello.py
+++ hello.py
@@ -5,12 +5,13 @@ from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from wtforms.widgets import TextArea
 
 
 # create a flask instance
 app = Flask(__name__)
 # Add Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///User1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Secret key password enviromental variable
 app.config['SECRET_KEY'] = os.environ.get("DB_PASS") 
@@ -23,25 +24,52 @@ class Users(db.Model):
     name = db.Column(db.String(200),nullable=False)
     email = db.Column(db.String(120),nullable=False,unique=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    blog = db.relationship('Blog', backref='user', lazy=True)
+    
 
     def __repr__(self):
      return '<Name %r>' % self.name
 
 
-
-class Blog(db.Model):
+# Create another table
+class Posts(db.Model):
    id = db.Column(db.Integer, primary_key = True) 
-   name = db.Column(db.String(250),nullable=False)
    title = db.Column(db.String(250),nullable=False)
    content = db.Column(db.Text,nullable=False)
    author = db.Column(db.String(250),nullable=False)
-   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-   date_added = db.Column(db.DateTime, default=datetime.utcnow)
+   date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+   slug = db.Column(db.String(255))
 
    def __repr__(self):
      return '<Name %r>' % self.name
 
+# Create a new form for db
+class PostForm(FlaskForm):
+    title = StringField("Title:", validators=[DataRequired()])
+    content = StringField("Content:", validators=[DataRequired()],widget=TextArea())
+    author = StringField("Author:", validators=[DataRequired()])
+    slug = StringField("Slug:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Add Post Page
+@app.route('/add-post', methods=['GET','POST'])
+def add_post():
+    form=PostForm()
+
+    if form.validate_on_submit():
+      post = Posts(title=form.title.data,content=form.content.data,author=form.author.data,slug=form.slug.data)
+     #Clear Form
+      form.title.data = ''
+      form.content.data = ''
+      form.author.data = ''
+      form.slug.data = ''
+      #Add to DB
+      db.session.add(post)
+      db.session.commit()
+
+      flash("Blog Post Submitted")
+
+    return render_template("add_post.html",form=form)
+   
 
 # Delete Users in Database
 @app.route('/delete/<int:id>')
