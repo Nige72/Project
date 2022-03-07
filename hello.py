ed958bd9520bfb4451cbc97f16b1444d2858716a
Nigel Squire <squiren72@gmail.com>
2022-03-07 18:29:21 +0000

    NS: Added login page & dashboard pages and functionality

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from wtforms.widgets import TextArea
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user, user_loaded_from_request 
 
 # create a flask instance
 app = Flask(__name__)
@@ -19,6 +19,75 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get("DB_PASS") 
 # initialize database
 db=SQLAlchemy(app)
+
+# Flask Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+#### FORMS ######
+# Create LoginForm
+class LoginForm(FlaskForm):
+    username = StringField("Username:", validators=[DataRequired()])
+    password = PasswordField("Password:", validators=[DataRequired()])
+    submit = SubmitField("Submit")  
+# Create Password Form
+class PassForm(FlaskForm):
+    email = StringField("Email:", validators=[DataRequired()])
+    password_hash = PasswordField("Password:", validators=[DataRequired()])
+    submit = SubmitField("Submit")  
+# Create Name Form
+class NamerForm(FlaskForm):
+    name = StringField("Name:", validators=[DataRequired()])
+    submit = SubmitField("Submit")       
+# Create a new User form for db
+class UserForm(FlaskForm):
+    name = StringField("Name:",validators=[DataRequired()])
+    username = StringField("Username:", validators=[DataRequired()])
+    email = StringField("Email:", validators=[DataRequired()])
+    password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo("password_hash2", message = "Passwords Must Match")])
+    password_hash2 = PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+# Create a Posts form for db
+class PostForm(FlaskForm):
+    title = StringField("Title:", validators=[DataRequired()])
+    content = StringField("Content:", validators=[DataRequired()],widget=TextArea())
+    author = StringField("Author:", validators=[DataRequired()])
+    slug = StringField("Slug:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+# Create Login Page
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+# Check the Hash
+            if check_password_hash(user.password_hash, form.password.data): 
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+               flash("Wrong Password Matey") 
+        else: 
+            flash("That User Does not exist try again matey!!!!")   
+    return render_template('login.html',form=form)
+# Create Logout
+@app.route('/logout', methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("See ya matey you are logged out!!!!")  
+    return redirect(url_for('login'))
+# Create Dashboard Page
+@app.route('/dashboard', methods=['GET','POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
 # Create Posts table
 class Posts(db.Model):
    id = db.Column(db.Integer, primary_key = True) 
@@ -55,15 +124,6 @@ class Users(db.Model,UserMixin):
     def __repr__(self):
      return '<Name %r>' % self.name
 
-###
-
-# Create a Posts form for db
-class PostForm(FlaskForm):
-    title = StringField("Title:", validators=[DataRequired()])
-    content = StringField("Content:", validators=[DataRequired()],widget=TextArea())
-    author = StringField("Author:", validators=[DataRequired()])
-    slug = StringField("Slug:", validators=[DataRequired()])
-    submit = SubmitField("Submit")
 # Delete Posts
 @app.route('/posts/delete/int<id>')
 def delete_posts(id):
@@ -146,22 +206,6 @@ def delete(id):
     except:
         flash("There was a problem")
         return render_template("add_user.html",form=form,name=name,our_users=our_users)
-# Create Password Form
-class PassForm(FlaskForm):
-    email = StringField("Email:", validators=[DataRequired()])
-    password_hash = PasswordField("Password:", validators=[DataRequired()])
-    submit = SubmitField("Submit")  
-# Create Name Form
-class NamerForm(FlaskForm):
-    name = StringField("Name:", validators=[DataRequired()])
-    submit = SubmitField("Submit")       
-# Create a new User form for db
-class UserForm(FlaskForm):
-    name = StringField("Name:", validators=[DataRequired()])
-    email = StringField("Email:", validators=[DataRequired()])
-    password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo("password_hash2", message = "Passwords Must Match")])
-    password_hash2 = PasswordField("Confirm Password", validators=[DataRequired()])
-    submit = SubmitField("Submit")
 
 # Update Database Record
 @app.route('/update/<int:id>',methods=['GET','POST'])
@@ -194,11 +238,12 @@ def add_user():
         if user is None:
 # Password hash
             hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
-            user = Users(name=form.name.data, email=form.email.data, password_hash= hashed_pw)
+            user = Users(name=form.name.data,username=form.username.data, email=form.email.data, password_hash= hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
+        form.username.data = ''
         form.email.data = ''
         form.password_hash.data = ''
         flash("User added to Database")
